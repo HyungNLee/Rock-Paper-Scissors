65831e55b274d39e07ae7f7b47f965b9f236ac96
Brian Nelson and Hyung Lee <brian@>
2018-09-06 14:32:38 -0700

    Finish functions and tests.

--- RockPaperScissors.Tests/ModelTests/RpsTests.cs
+++ RockPaperScissors.Tests/ModelTests/RpsTests.cs
@@ -7,16 +7,55 @@ namespace RPS.Tests
   public class RPSTest
   {
     [TestMethod]
-    public void GetChoice_IsARock_True()
+    public void GetPlayerNumbers_1_1()
     {
       // Arrange
-      string sample = "Rock";
-      Game newGame = new Game();
+      int player1 = 1;
+      int player2 = 2;
 
       // Act
-      string result = newGame.GetChoice();
+      Game newGame = new Game("paper", "scissors");
+      int first = newGame.GetPlayer1Number();
+      int second = newGame.GetPlayer2Number();
       // Assert
-      Assert.AreEqual(sample, result);
+      // Assert.AreEqual(first, player1);
+      Assert.AreEqual(second, player2);
+    }
+
+    [TestMethod]
+    public void DetermineWinner_Player1_1to3()
+    {
+      // Arrange
+      Game newGame = new Game("rock", "scissors");
+
+      // Act
+      string result = newGame.DetermineWinner();
+      // Assert
+      Assert.AreEqual("Player One Wins!", result);
+    }
+
+    [TestMethod]
+    public void DetermineWinner_Tie()
+    {
+      // Arrange
+      Game newGame = new Game("rock", "rock");
+
+      // Act
+      string result = newGame.DetermineWinner();
+      // Assert
+      Assert.AreEqual("It is a draw!", result);
+    }
+
+    [TestMethod]
+    public void DetermineWinner_Player1_2to3()
+    {
+      // Arrange
+      Game newGame = new Game("paper", "scissors");
+
+      // Act
+      string result = newGame.DetermineWinner();
+      // Assert
+      Assert.AreEqual("Player Two Wins!", result);
     }
   }
 }
--- RockPaperScissors/Models/Rps.cs
+++ RockPaperScissors/Models/Rps.cs
@@ -1,16 +1,53 @@
 using System;
-
+using System.Collections.Generic;
 
 namespace RPS
 {
-  class Game
+  public class Game
   {
-    private string _inputChoice;
-
-    public string GetChoice()
+    private int _player1Number;
+    private int _player2Number;
+    private Dictionary<string, int> handValue = new Dictionary<string, int> ()
     {
-      return "Scissor"; //"Rock";
+      { "rock", 0 },
+      { "paper", 1 },
+      { "scissors", 2 }
+    };
+
+
+    public Game(string player1, string player2)
+    {
+      _player1Number = handValue[player1];
+      _player2Number = handValue[player2];
     }
+
+    public int GetPlayer1Number()
+    {
+      return _player1Number;
+
+    }
+    public int GetPlayer2Number()
+    {
+      return _player2Number;
+    }
+
+    public string DetermineWinner()
+    {
+      int difference = Math.Abs(_player1Number - _player2Number);
+      if (difference == 2)
+      {
+        return (_player1Number < _player2Number) ? "Player One Wins!" : "Player Two Wins!";
+      }
+      else if (difference == 1)
+      {
+        return (_player1Number > _player2Number) ? "Player One Wins!" : "Player Two Wins!";
+      }
+      else
+      {
+        return "It is a draw!";
+      }
+    }
+
   }
 
   public class Program
